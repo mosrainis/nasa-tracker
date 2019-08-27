35d5957f2f28ce00c2957ab012d6ed7fdb16305e
mosrainis <mostafaes5520@gmail.com>
2019-08-27 17:42:04 +0430

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApplayoutComponent } from './components/applayout/applayout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApplayoutComponent,
+    HeaderComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/applayout/applayout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applayout',
+  templateUrl: './applayout.component.html',
+  styleUrls: ['./applayout.component.scss']
+})
+export class ApplayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
